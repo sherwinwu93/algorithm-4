80b7e7571ac26a08726e001c7e7838e71422f681
wusd <wsd5656126@foxmail.com>
2022-03-29 19:51:15 +0800

    背包`队列`栈

---
+++ src/cat10/P080Evaluate.java
@@ -0,0 +1,36 @@
+package cat10;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Dijkstra双栈算法表达式求值算法
+ */
+public class P080Evaluate {
+    // 计算模型: 将一个字符串解释为一段程序并执行程序得到结果, puns
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<String>();
+        Stack<Double> vals = new Stack<Double>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("(")) ;
+            else if (s.equals("+")) ops.push(s);
+            else if (s.equals("-")) ops.push(s);
+            else if (s.equals("*")) ops.push(s);
+            else if (s.equals("/")) ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")")) {
+                String op = ops.pop();
+                double v = vals.pop();
+                if (op.equals("+")) v = vals.pop() + v;
+                else if (op.equals("-")) v = vals.pop() - v;
+                else if (op.equals("*")) v= vals.pop() * v;
+                else if (op.equals("/")) v = vals.pop() / v;
+                else if (op.equals("sqrt")) v = Math.sqrt(v);
+                vals.push(v);
+            } else vals.push(Double.parseDouble(s));
+        }
+        StdOut.println(vals.pop());
+    }
+}
---
+++ src/cat10/P082FixedCapacityStackOfStrings.java
@@ -0,0 +1,41 @@
+package cat10;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 数组中元素顺序与被插入顺序相同
+ * 当N为0时栈为空
+ * 栈的顶部位于a[N-1]
+ */
+public class P082FixedCapacityStackOfStrings {
+    private String[] a;//stack entries
+    private int N; //size
+    public P082FixedCapacityStackOfStrings(int cap) {
+        a = new String[cap];
+    }
+    void push(String item) {
+        a[N++] = item;
+    }
+    String pop() {
+        return a[--N];
+    }
+    boolean isEmpty() {
+        return N == 0;
+    }
+    int size() {
+        return N;
+    }
+    //测试用例
+    public static void main(String[] args) {
+        P082FixedCapacityStackOfStrings s;
+        s = new P082FixedCapacityStackOfStrings(100);
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                s.push(item);
+            else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
