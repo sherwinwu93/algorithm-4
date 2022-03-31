9c7c9743348e275fc7daa9c1cb4145d93a790506
wusd <wsd5656126@foxmail.com>
2022-03-31 19:40:47 +0800

    消息队列

--- src/cat10/ex3/P10339RingBuffer.java
+++ src/cat10/ex3/P10339RingBuffer.java
@@ -1,6 +1,7 @@
 package cat10.ex3;
 
 import cat10.P095Queue;
+import edu.princeton.cs.algs4.StdOut;
 
 public class P10339RingBuffer<Item> {
     private Item[] ringBuffer;
@@ -16,13 +17,51 @@ public class P10339RingBuffer<Item> {
     }
 
     public void produce(Item item) {
+        if (consumedCnt > 0) {
+            consumeDate(item);
+        } else if (isFull()) {
+            freeze.enqueue(item);
+        } else {
+            ringBuffer[tail] = item;
+            N++;
+            nextTail();
+        }
     }
-    public Item consume() {
 
+    private void nextTail() {
+        if (tail == ringBuffer.length - 1) tail = 0;
+        else tail++;
     }
+
+    private void nextHead() {
+        if (head == ringBuffer.length - 1) head = 0;
+        else head++;
+    }
+
+    private void consumeDate(Item item) {
+        consumedCnt--;
+        StdOut.println(item);
+    }
+
+    public void consume() {
+        if (isEmpty()) consumedCnt++;
+        else {
+            Item item = ringBuffer[head];
+            nextHead();
+            consumeDate(item);
+            fill();
+        }
+    }
+
+    private void fill() {
+        while (!isFull() && freeze.isEmpty())
+            ringBuffer[tail] = freeze.dequeue();
+    }
+
     public boolean isEmpty() {
         return N == 0;
     }
+
     public boolean isFull() {
         return N == ringBuffer.length;
     }
