8860f3d6dd9fa2c62b09ec40daa3b6f6696aa71a
wusd <wsd5656126@foxmail.com>
2022-04-21 00:04:04 +0800

    --

--- src/cat20/ex1/Ex20137PartiallySorted.java
+++ src/cat20/ex1/Ex20137PartiallySorted.java
@@ -1,6 +1,7 @@
 package cat20.ex1;
 
 import edu.princeton.cs.algs4.StdRandom;
+import lib.PrintUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,8 @@ public class Ex20137PartiallySorted {
         // 95%有序
         // 所有元素与正确位置的距离不超过10
         // 5%的元素随机分布在整个数组中,剩下数据都是有序的
+        int[] a = distance10Array();
+        PrintUtils.printArray(a);
     }
     // 95%有序
     private static int[] percentArray() {
@@ -31,7 +34,23 @@ public class Ex20137PartiallySorted {
         int[] a = new int[N];
         for (int i = 0; i < N; i++)
             a[i] = i;
-        boolean[] b = new boolean[N];
+        List<Integer> exchedList = new ArrayList<Integer>();
+        List<Integer> randomList = new ArrayList<Integer>();
+        for (int i = 0; i < N; i++) {
+            if (exchedList.contains(i)) continue;
+            randomList.clear();
+            int low = i - 10 >= 0? i-10: 0;
+            int hi = i + 10 < N? i+10: N -1;
+            for (int j = low; j <=hi; j++)
+                if (exchedList.contains(j)) continue;
+                else randomList.add(j);
+            int exch = randomList.get(StdRandom.uniform(0, randomList.size()));
+            int temp = a[i];
+            a[i] = a[exch];
+            a[exch] = temp;
+            exchedList.add(a[i]);
+            exchedList.add(a[exch]);
+        }
         return a;
     }
     private static int[] uniform() {
