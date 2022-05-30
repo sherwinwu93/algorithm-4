bd405640246a179cc402d30f966f3c49cccb1cb8
wusd <wsd5656126@foxmail.com>
2022-05-30 23:22:25 +0800

    --

--- src/cat20/P182Quick.java
+++ src/cat20/P182Quick.java
@@ -1,11 +1,12 @@
 package cat20;
 
+import edu.princeton.cs.algs4.StdRandom;
 import lib.PrintUtils;
 
 /**
  * Created by Wusd on 2022/5/30.
  **/
-public class P139Quick3way {
+public class P182Quick {
     public static void main(String[] args) {
         Integer[] a = {5,4,3,1,2,7,8,6,5};
         sort(a);
@@ -17,12 +18,23 @@ public class P139Quick3way {
     }
     private static void sort(Comparable[] a, int lo, int hi) {
         if (lo >= hi) return;
-        int lt = lo;
-        int i = lo + 1;
-        int gt = hi;
-        while (i <= gt) {
-
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j+1, hi);
+    }
+    // 将数组切分为a[lo...i-1],a[i],a[i+1...hi]
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
         }
+        exch(a, lo, j);
+        return j;
     }
     private static boolean less(Comparable u, Comparable v) {
         return u.compareTo(v) < 0;
--- src/cat20/P189Quick3way.java
+++ src/cat20/P189Quick3way.java
@@ -1,12 +1,11 @@
 package cat20;
 
-import edu.princeton.cs.algs4.StdRandom;
 import lib.PrintUtils;
 
 /**
  * Created by Wusd on 2022/5/30.
  **/
-public class P194Quick {
+public class P189Quick3way {
     public static void main(String[] args) {
         Integer[] a = {5,4,3,1,2,7,8,6,5};
         sort(a);
@@ -18,23 +17,20 @@ public class P194Quick {
     }
     private static void sort(Comparable[] a, int lo, int hi) {
         if (lo >= hi) return;
-        int j = partition(a, lo, hi);
-        sort(a, lo, j - 1);
-        sort(a, j+1, hi);
-    }
-    // 将数组切分为a[lo...i-1],a[i],a[i+1...hi]
-    private static int partition(Comparable[] a, int lo, int hi) {
-        int i = lo;
-        int j = hi + 1;
+        int lt = lo;
+        int i = lo + 1;
+        int gt = hi;
         Comparable v = a[lo];
-        while (true) {
-            while (less(a[++i], v)) if (i == hi) break;
-            while (less(v, a[--j])) if (j == lo) break;
-            if (i >= j) break;
-            exch(a, i, j);
+        // <的归左边,>的归右边,=的扩大中间
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a,  i++, lt++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else i++;
         }
-        exch(a, lo, j);
-        return j;
+        // 这里要特别小心,不能取相等的地方
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
     }
     private static boolean less(Comparable u, Comparable v) {
         return u.compareTo(v) < 0;
