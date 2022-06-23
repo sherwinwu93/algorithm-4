cbb8e754a91381ef4447aada968338d0fc0f1b29
wusd <wsd5656126@foxmail.com>
2022-06-23 09:57:13 +0800

    优先队列->堆排序

---
+++ src/cat20/ex2/Ex20409.java
@@ -0,0 +1,12 @@
+package cat20.ex2;
+
+/**
+ * Created by Wusd on 2022/6/21.
+ * 给出ABCDE可能构造出来的所有堆,然后给出AAABB五个元素可能构造出来的所有堆
+ **/
+public class Ex20409 {
+    /**
+     * ABCDE 6种
+     * AAABB 2种
+     */
+}
