ed5b699ae315f62b8ee732bbeca4aabb4fd4897f
shengdong.wu <shengdong.wu@chinaxpp.com>
2026-05-18 15:21:38 +0800

    --

--- src/ch04/Bridge.java
+++ src/ch04/Bridge.java
@@ -67,12 +67,15 @@ public class Bridge {
         testCyclic();
     }
 
+    /**
+     * 算法4 P533看具体的执行情况, 有空的时候画一画
+     */
     private static void testCyclic() {
         Graph G = new Graph(4);
-        G.addEdge(3, 0);
         G.addEdge(0, 1);
         G.addEdge(1, 2);
         G.addEdge(2, 3);
+        G.addEdge(3, 0);
 
         Bridge bridge = new Bridge(G);
         StdOut.println("Edge connected components = " + bridge.components());
